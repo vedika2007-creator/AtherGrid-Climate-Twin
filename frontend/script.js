e8f121671bd6a212792c97581237862fca09b92a
Vedika Bomble <vedikabomble2803@gmail.com>
2026-06-27 16:06:43 +0530

    frontend done

--- frontend/script.js
+++ frontend/script.js
@@ -42,3 +42,50 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 
 });
+
+// ===============================
+// AtherGrid India Climate Map
+// ===============================
+
+const mapElement = document.getElementById("map");
+
+if (mapElement) {
+
+    const map = L.map("map").setView([22.9734, 78.6569], 5);
+
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+        attribution: "&copy; OpenStreetMap & CARTO"
+    }).addTo(map);
+
+    // Current marker
+let currentMarker = null;
+
+// When user clicks on the map
+map.on("click", function (e) {
+
+    const lat = e.latlng.lat.toFixed(4);
+    const lng = e.latlng.lng.toFixed(4);
+
+    document.getElementById("location-name").textContent =
+`Latitude: ${lat}, Longitude: ${lng}`;
+
+    // Remove previous marker
+    if (currentMarker) {
+        map.removeLayer(currentMarker);
+    }
+
+    // Create new marker
+    currentMarker = L.marker([lat, lng]).addTo(map);
+
+    currentMarker.bindPopup(`
+        <b>Selected Location</b><br>
+        Latitude: ${lat}<br>
+        Longitude: ${lng}
+    `).openPopup();
+
+    // Temporary values (will come from Python later)
+    document.getElementById("temperature").textContent = "-- °C";
+    document.getElementById("rainfall").textContent = "-- mm";
+    document.getElementById("humidity").textContent = "-- %";
+    document.getElementById("risk").textContent = "Loading...";
+});}
